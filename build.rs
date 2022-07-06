628f7f82fbf31b754752398c35b5adabf63fe7e3
user-vsssfurg <107590981+user-vsssfurg@users.noreply.github.com>
2022-07-06 19:25:32 -0300

    Add files via upload

--- build.rs
+++ build.rs
@@ -3,7 +3,10 @@ extern crate prost_build;
 use std::{result::Result, io::Error};
 
 fn main() -> Result<(), Error> {
-    prost_build::compile_protos(&["src/common.proto", "src/command.proto", "src/packet.proto", "src/replacement.proto"], &["src"])?;
+    prost_build::compile_protos(
+        &["src/protos/common.proto", "src/protos/command.proto", "src/protos/packet.proto", "src/protos/replacement.proto"], 
+        &["src/protos"]
+    )?;
 
     Ok(())
 }
