362d51edac4452fddda8e51c0a80680d70a2719c
user-vsssfurg <107590981+user-vsssfurg@users.noreply.github.com>
2022-07-06 17:11:59 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -71,19 +71,16 @@ use std::net::UdpSocket;
 use std::time::Duration;
 use std::thread::sleep;
 
-// use std::sync::{Arc, Mutex};
-// use bytes::Bytes;
-
-
 use fbot_vss_rust::{fira_protos, deserialize_env, serialize_packet};
-const VISION_ADDRS: &str = "244.0.0.1:10002";
-const COMMAND_ADDRS: &str = "127.0.0.1:20011";
+const VISION_ADDRS: &str = "224.0.0.1:10002";
+const COMMAND_ADDRS: &str = "127.0.0.1:10011";
 
 // type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 // #[tokio::main]
 fn main() {
-    let socket = UdpSocket::bind(VISION_ADDRS).unwrap();
+    let send_socket = UdpSocket::bind(COMMAND_ADDRS).unwrap();
+
     println!("Listening to {}", VISION_ADDRS);
 
     let packet = fira_protos::Packet {
@@ -113,25 +110,12 @@ fn main() {
     };
     let buf_command = serialize_packet(&packet);
 
-    // socket.connect(COMMAND_ADDRS).expect("connect function failed");
-    loop {        
-        socket.send_to(&buf_command, COMMAND_ADDRS).expect("send erro");
-        println!("Send {:?}", a);
-
-        let mut buf = [0; 1024];
-
-        let (len, addr) = socket.recv_from(&mut buf).unwrap();
-        println!("{:?} bytes received from {:?}", len, addr);
-        
-
-        let env = deserialize_env(&buf[..len]).unwrap();
-
-        if let Some(frame) = env.frame {
-            println!("{:?}", frame.ball);    
+    loop {
+        if let Some(ball) = fbot_vss_rust::get_ball() {
+            println!("x: {:?}", ball.x);
+            println!("y: {:?}", ball.y);
         }
-
-
-
+        
         sleep(Duration::from_millis(100));
     }
 }
