bc7b202aa2583cbfd062744aa4d4f294bb2e4e5b
LucasMartinsUthi <luscagbr@gmail.com>
2022-07-09 22:25:14 -0300

    teste novo pc

--- src/main.rs
+++ src/main.rs
@@ -7,22 +7,23 @@ fn main() {
 
         // TODO Garantir retorno do goalie
         if let Some(goalie)  = get_blue_robot(0) {
-            let err = ball.y - goalie.y;
+            println!("{}", goalie.orientation)
+            // let err = ball.y - goalie.y;
 
-            let velocidade = err * 1000.0;
+            // let velocidade = err * 1000.0;
 
-            let commands = fira_protos::Commands {
-                robot_commands: vec![
-                    fira_protos::Command {
-                        id: 3,
-                        yellowteam: false,
-                        wheel_left: velocidade,
-                        wheel_right: velocidade
-                    },
-                ]
-            };
+            // let commands = fira_protos::Commands {
+            //     robot_commands: vec![
+            //         fira_protos::Command {
+            //             id: 3,
+            //             yellowteam: false,
+            //             wheel_left: velocidade,
+            //             wheel_right: velocidade
+            //         },
+            //     ]
+            // };
         
-            send_command(commands);
+            // send_command(commands);
         };
     }
 }
